824be014967bfc67ec72cf14511602efca38f9b7
FloraBella1427 <ayundabella1412>
2024-05-24 19:51:23 +0700

    kode flo

---
+++ resources/views/admin/petugasA-add.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="main">
+        <nav class="navbar navbar-dark navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <!-- Ensure the file path is correct -->
+                    <img src="{{ asset('images/2.png') }}" alt="Logo" style="height: 40px; width: auto;">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <form class="d-flex me-auto" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    </form>
+                    <div class="flora d-flex align-items-center">
+                        <div class="rounded-circle overflow-hidden me-2" style="width: 35px; height: 35px;">
+                            <img src="{{ asset('images/p.jpeg') }}" alt="Profile" class="img-fluid">
+                        </div>
+                        <span class="text-light-1">Shadiq Usep</span>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div class="body-content">
+            <div class="sidebar d-lg-block collapse" id="navbarTogglerDemo02">
+                <a href="homeA">Dashboard</a>
+                <a href="petugasA" class="active">Petugas</a>
+                <a href="">User</a>
+                <a href="loginA">Log out</a>
+            </div>
+            <div class="content">
+                <h2>Add User Petugas</h2>
+
+                <div class="d-flex justify-content-end mt-3">
+                    <a href="{{ route('petugasA') }}" class="btn btn-secondary me-5">Back</a>
+                </div>
+
+                <div class="mt-5 w-50">
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endif
+
+                    <form action="">
+                        @csrf
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group mb-3">
+                                    <label for="book_code" class="form-label">Petugas Code</label>
+                                    <input type="text" name="book_code" id="book_code" class="form-control" placeholder="Enter Petugas Code" required>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="form-group mb-3">
+                                    <label for="kelamin" class="form-label">Kelamin</label>
+                                    <select name="kelamin" id="kelamin" class="form-control select-multiple" required>
+                                        <option value="Laki-Laki">Laki-Laki</option>
+                                        <option value="Perempuan">Perempuan</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <label for="username" class="form-label">Username</label>
+                                <input type="text" name="username" id="username" class="form-control" placeholder="Enter Username" required>
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <label for="password" class="form-label">Password</label>
+                                <input type="password" name="password" id="password" class="form-control" placeholder="Enter Password" required>
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <label for="phone" class="form-label">No. Handphone</label>
+                                <input type="text" name="phone" id="phone" class="form-control" placeholder="Enter No. Handphone" required>
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script>
+        $(document).ready(function() {
+            $('.select-multiple').select2();
+        });
+    </script>
+</body>
+</html>
