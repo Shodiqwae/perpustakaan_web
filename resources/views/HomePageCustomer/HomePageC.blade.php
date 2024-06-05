c2c65755106d342e32abc1200d257db021424b97
FloraBella1427 <ayundabella1412>
2024-06-05 08:34:51 +0700

    hehheeh

--- resources/views/HomePageCustomer/HomePageC.blade.php
+++ resources/views/HomePageCustomer/HomePageC.blade.php
@@ -214,8 +214,8 @@
                                 <input type="file" class="form-control" id="profile-picture" name="profile-picture" accept="image/*">
                             </div>
                             <div class="mb-3">
-                                <label for="email" class="form-label">Username</label>
-                                <input type="email" class="form-control" id="email" name="email">
+                                <label for="username" class="form-label">Username</label>
+                                <input type="username" class="form-control" id="username" name="username">
                             </div>
 
                             <button type="submit" class="btn btn-primary">Save</button>
